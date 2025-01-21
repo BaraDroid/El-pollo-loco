b486deb61e4e3ab642be15f89deacc655fd36ae8
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-01-21 14:35:56 +0100

    sky added

--- scripts/models/character.class.js
+++ scripts/models/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MovableObject {
-    height = 400;
-    width = 150;
-    y = 43;
+    height = 280;
+    width = 130;
+    y = 153;
     constructor(){
         super().loadImage('img_pollo_locco/img/2_character_pepe/2_walk/W-21.png');
     }
--- scripts/models/world.class.js
+++ scripts/models/world.class.js
@@ -8,6 +8,7 @@ class World {
     clouds = [new Cloud()];
     canvas;
     backgroundObjects = [
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/air.png', 0, 0),
       new BackgroundObject('img_pollo_locco/img/5_background/layers/3_third_layer/1.png', 0, 0),
       new BackgroundObject('img_pollo_locco/img/5_background/layers/2_second_layer/1.png', 0, 0), 
       new BackgroundObject('img_pollo_locco/img/5_background/layers/1_first_layer/1.png', 0, 0) 
