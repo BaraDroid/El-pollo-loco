d632d447421036831499eb57d37be039be4e732a
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-01-21 07:28:06 +0100

    first pictures of character and chicken

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,12 @@
 let canvas;
 let ctx;
-let character = new movableObject();
+
+let world = new World();
 
 function init() {   //die bindet unser Canvas an einer Variablen und dann fügen wir das BIld hinzu
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext('2d');
 
-    console.log("My Character is", character);
+    console.log("My Character is", world["character"]);
     
 }
--- scripts/models/movable-object.class.js
+++ scripts/models/movable-object.class.js
@@ -1,9 +1,18 @@
-class movableObject{
+class MovableObject{
     x = 120;
     y = 300;
     img;
 
+    loadImage(path) {
+        this.img = new Image(); 
+        this.img.src = path;
+    }
+
     moveRight() {
         console.log("moving right");
+    }
+
+    moveLeft() {
+        
     }
 }
