e0001b73470deb08cad242ed4dce9a6675756a11
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-01-23 09:30:20 +0100

    background images for moving camera added

--- scripts/models/world.class.js
+++ scripts/models/world.class.js
@@ -8,10 +8,32 @@ class World {
     clouds = [new Cloud()];
     canvas;
     backgroundObjects = [
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/air.png', -719*2, 0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/3_third_layer/1.png', -719*2, 0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/2_second_layer/1.png', -719*2, 0), 
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/1_first_layer/1.png', -719*2, 0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/air.png', -719, 0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/3_third_layer/2.png', -719,0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/2_second_layer/2.png', -719, 0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/1_first_layer/2.png', -719, 0),
+
       new BackgroundObject('img_pollo_locco/img/5_background/layers/air.png', 0, 0),
       new BackgroundObject('img_pollo_locco/img/5_background/layers/3_third_layer/1.png', 0, 0),
       new BackgroundObject('img_pollo_locco/img/5_background/layers/2_second_layer/1.png', 0, 0), 
-      new BackgroundObject('img_pollo_locco/img/5_background/layers/1_first_layer/1.png', 0, 0) 
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/1_first_layer/1.png', 0, 0), 
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/air.png', 719, 0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/3_third_layer/2.png', 719,0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/2_second_layer/2.png', 719, 0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/1_first_layer/2.png', 719, 0),
+
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/air.png', 719*2, 0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/3_third_layer/1.png', 719*2, 0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/2_second_layer/1.png', 719*2, 0), 
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/1_first_layer/1.png', 719*2, 0), 
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/air.png', 719*3, 0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/3_third_layer/2.png', 719*3,0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/2_second_layer/2.png', 719*3, 0),
+      new BackgroundObject('img_pollo_locco/img/5_background/layers/1_first_layer/2.png', 719*3, 0)
     ];
     ctx;
     keyboard;
