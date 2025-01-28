7cb36db654aa6b694928c95c7c526d6d488636d0
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-01-28 10:14:14 +0100

    animation of bottle rotation added

--- scripts/models/collectable-object.class.js
+++ scripts/models/collectable-object.class.js
@@ -4,18 +4,26 @@ class CollectableObject extends DrawableObject {
     height = 60;
     width = 80;
     level;
-    IMAGES_BOTTLES_ON_GROUND = [
+    IMAGES_BOTTLES_ONGROUND = [
         'img_pollo_locco/img/6_salsa_bottle/1_salsa_bottle_on_ground.png',
         'img_pollo_locco/img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
     ];
+ 
+
 
     constructor(level) {
-        super().loadImage('img_pollo_locco/img/6_salsa_bottle/1_salsa_bottle_on_ground.png');
+        super().loadImage(this.IMAGES_BOTTLES_ONGROUND[this.getBottleImage()]);
         this.level = level;
         this.x = this.level.level_end_x - Math.random()*2500;
+        this.getBottleImage();
     }
 
     distributeBottles() {
         this.x = this.level.level_end_x - Math.random()*2800;
+    }
+
+    getBottleImage() {
+        let randomIndex = Math.floor(Math.random()*2);
+        return randomIndex;
     }
 }
--- scripts/models/throwable-object.class.js
+++ scripts/models/throwable-object.class.js
@@ -2,9 +2,18 @@ class ThrowableObject extends MovableObject{
     height = 60;
     width = 60;
     accelaration = 5;
+    bottleBroke = new Audio('audio/glass_broken.mp3')
+
+    IMAGES_THROWN = [
+        'img_pollo_locco/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img_pollo_locco/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img_pollo_locco/img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img_pollo_locco/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
+    ];
 
 constructor(x, y) {
     super().loadImage('img_pollo_locco/img/6_salsa_bottle/salsa_bottle.png');
+    this.loadImages(this.IMAGES_THROWN);
     this.x = x;
     this.y = y;
     this.throw();
@@ -14,7 +23,10 @@ throw() {
     this.speedY = 45;
     this.applyGravity();
     setInterval(() => {
+        this.playAnimation(this.IMAGES_THROWN);
         this.x += 25;
-    }, 100);
+    }, 1000/15);
+    
 }
+
 }
