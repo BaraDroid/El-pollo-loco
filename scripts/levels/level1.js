4f6c718028a4ce0b8f8590fad5274883db0a5807
Barbora Lambeinova <lambeinovab@seznam.cz>
2025-01-29 08:21:12 +0100

    baby chickens added

--- scripts/levels/level1.js
+++ scripts/levels/level1.js
@@ -3,7 +3,9 @@ const level1 = new Level(
         new Chicken(),
         new Chicken(),
         new Chicken(),
-        new Endboss()
+        new Endboss(),
+        new Babychicken(),
+        new Babychicken()
     ],
     [new Cloud()],
     [
---
+++ scripts/models/baby-chicken.class.js
@@ -0,0 +1,25 @@
+class Babychicken extends Chicken {
+    y = 385;
+    height = 50;
+    width = 50;
+    offset = {
+        top: 10,
+        bottom: 20,
+        left: 10,
+        right: 20
+    }
+
+    IMAGES_WALKING =[
+        'img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+    ];
+
+    constructor() {
+        super().loadImage('img_pollo_locco/img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+        this.x = 720 + Math.random()*500;
+        this.speed = 0.17 + Math.random()*0.5;
+        this.loadImages(this.IMAGES_WALKING);
+        this.animate();
+    }
+}
